0b8d6085d17b678bbbd235002017d8cb7f8c59d4
Anush Bhatia <anushbhatia1234@gmail.com>
2021-01-23 12:42:51 +0530

    Added Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Container from './Navbar'
 export default function Home() {
   return (
+
     <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+            <Container />
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Hey 👋 , Anush Here
         </h1>
@@ -10,5 +13,6 @@ export default function Home() {
         I have been ardent practitioner of cloud computing for over 4 years and have experience across multiple platforms. On the other hand , I am active UI/UX designer, having worked with multiple startups to revamp their products into a more appealing look. As far as full stack goes, I have had my fair share of work in majority of the stacks like MERN,MEAN etc.
         </h2>
       </div>
-  )
+
+    )
 }
